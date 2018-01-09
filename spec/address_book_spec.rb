aecf7cc349a0fe3b46483e143ca74c199c4199a2
Lucas Cuba <lucas.cuba4@gmail.com>
2018-01-09 19:15:40 +0000

    checkpoint 6 assignment - added support for testing second csv file

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -77,22 +77,48 @@
             check_entry(entry_two, "Bob", "555-555-5415", "bob@blocmail.com")
         end
  
-         it "imports the 3rd entry" do
+        it "imports the 3rd entry" do
             book.import_from_csv("entries.csv")
             entry_three = book.entries[2]
             check_entry(entry_three, "Joe", "555-555-3660", "joe@blocmail.com")
-         end
+        end
      
-         it "imports the 4th entry" do
+        it "imports the 4th entry" do
             book.import_from_csv("entries.csv")
             entry_four = book.entries[3]
             check_entry(entry_four, "Sally", "555-555-4646", "sally@blocmail.com")
-         end
+        end
      
-         it "imports the 5th entry" do
+        it "imports the 5th entry" do
             book.import_from_csv("entries.csv")
             entry_five = book.entries[4]
             check_entry(entry_five, "Sussie", "555-555-2036", "sussie@blocmail.com")
-     end
+        end
+        
+        context "importing from entries_2.csv"
+        it "imports the correct number of entries" do
+            book.import_from_csv("entries_2.csv")
+            
+            expect(book.entries.size).to eq 3
+        end
+        
+        it "imports the 1st entry" do
+            book.import_from_csv("entries_2.csv")
+            entry_one = book.entries[0]
+            
+            check_entry(entry_one, "Bill", "555-555-4854", "bill@blocmail.com")
+        end
+        
+        it "imports the 2nd entry" do
+            book.import_from_csv("entries_2.csv")
+            entry_two = book.entries[1]
+            check_entry(entry_two, "Bob", "555-555-5415", "bob@blocmail.com")
+        end
+ 
+        it "imports the 3rd entry" do
+            book.import_from_csv("entries_2.csv")
+            entry_three = book.entries[2]
+            check_entry(entry_three, "Joe", "555-555-3660", "joe@blocmail.com")
+        end
     end
  end
